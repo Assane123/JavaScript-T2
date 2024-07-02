f4f3c88c9396435545ad2e600fb0e6163c683264
Assane123 <132382386+Assane123@users.noreply.github.com>
2024-07-02 11:36:18 +0000

    Add files via upload

---
+++ variables/app.js
@@ -0,0 +1,16 @@
+// Types de Variables 
+
+let prenom =  'Gael' ; // String
+
+let age = 37; //Number 
+
+let choix = true; //Boolean
+
+let nulle = nulle; //Null
+
+let tableau = [1, 2, 3]; 
+//console  
+/* console.log(typeof prenom);
+console.log(typeof age);
+console.log(typeof enLigne);
+console.log(typeof choix); */
